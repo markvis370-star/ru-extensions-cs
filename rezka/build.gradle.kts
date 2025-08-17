7bf0cd05b2874dd394be05c7d59509ed97a52b9e
markvis370-star <markvis370@gmail.com>
2025-08-17 20:53:34 +0300

    Create build.gradle.kts

---
+++ rezka/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("cloudstream-plugin")
+}
+
+cloudstream {
+    language = "ru"
+    description = "Плагин для просмотра фильмов и сериалов с rezka"
+    authors = listOf("kiikuuchii")
+}
