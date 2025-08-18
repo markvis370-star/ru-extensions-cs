f85bb646747c024d568471f55d54662fbecb0505
markvis370-star <markvis370@gmail.com>
2025-08-18 09:42:19 +0300

    Delete rezka directory

--- rezka/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("cloudstream-plugin")
-}
-
-cloudstream {
-    language = "ru"
-    description = "Плагин для просмотра фильмов и сериалов с rezka.ua.org"
-    authors = listOf("kiikuuchii")
-}
